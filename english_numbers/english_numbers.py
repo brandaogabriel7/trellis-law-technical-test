3c0c6f390891566dbc910698386a521d40bb8e22
Gabriel Brandão <contato@gabrielbrandao.net>
2025-02-21 00:03:14 +0000

    feat: numbers up to 99

--- english_numbers/english_numbers.py
+++ english_numbers/english_numbers.py
@@ -21,6 +21,25 @@ numbers_in_english = {
     19: "nineteen"
 }
 
+tens_in_english = {
+    20: "twenty",
+    30: "thirty",
+    40: "forty",
+    50: "fifty",
+    60: "sixty",
+    70: "seventy",
+    80: "eighty",
+    90: "ninety"
+}
+
 
 def write_number_in_english(number: int) -> str:
-    return numbers_in_english[number]
+    if number > 19:
+        tens = number // 10 * 10
+        units = number % 10
+        if units:
+            return f"{tens_in_english[tens]}-{numbers_in_english[units]}"
+        else:
+            return tens_in_english[tens]
+    else:
+        return numbers_in_english[number]
--- english_numbers/test_english_numbers.py
+++ english_numbers/test_english_numbers.py
@@ -8,6 +8,43 @@ def test_numbers_from_0_to_9(number, number_in_english):
     assert write_number_in_english(number) == number_in_english
 
 
-@pytest.mark.parametrize("number,number_in_english", [(10, "ten"), (11, "eleven"), (12, "twelve"), (13, "thirteen"), (14, "fourteen"), (15, "fifteen"), (16, "sixteen"), (17, "seventeen"), (18, "eighteen"), (19, "nineteen")])
+@pytest.mark.parametrize("number,number_in_english", [
+    (10, "ten"),
+    (11, "eleven"),
+    (12, "twelve"),
+    (13, "thirteen"),
+    (14, "fourteen"),
+    (15, "fifteen"),
+    (16, "sixteen"),
+    (17, "seventeen"),
+    (18, "eighteen"),
+    (19, "nineteen")])
 def test_numbers_from_10_to_19(number, number_in_english):
+    assert write_number_in_english(number) == number_in_english
+
+
+@pytest.mark.parametrize("number,number_in_english", [
+    (20, "twenty"),
+    (21, "twenty-one"),
+    (22, "twenty-two"),
+    (23, "twenty-three"),
+    (24, "twenty-four"),
+    (25, "twenty-five"),
+    (26, "twenty-six"),
+    (27, "twenty-seven"),
+    (28, "twenty-eight"),
+    (29, "twenty-nine"),
+    (30, "thirty"),
+    (31, "thirty-one"),
+    (40, "forty"),
+    (42, "forty-two"),
+    (50, "fifty"),
+    (53, "fifty-three"),
+    (60, "sixty"),
+    (70, "seventy"),
+    (80, "eighty"),
+    (90, "ninety"),
+    (99, "ninety-nine")
+])
+def test_numbers_from_20_to_99(number, number_in_english):
     assert write_number_in_english(number) == number_in_english
