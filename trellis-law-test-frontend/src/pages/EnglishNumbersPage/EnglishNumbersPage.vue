c09f8bc8d0c5d44580403b96b7217913d912bb10
Gabriel Brandão <contato@gabrielbrandao.net>
2025-02-25 19:45:42 -0300

    style: add daisyui and tailwind to project

--- trellis-law-test-frontend/src/pages/EnglishNumbersPage/EnglishNumbersPage.vue
+++ trellis-law-test-frontend/src/pages/EnglishNumbersPage/EnglishNumbersPage.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { getEnglishNumber, postEnglishNumber } from '../../services/api';
+import { AxiosError } from 'axios';
 
 const number = ref<number | null>(0);
 const method = ref<'get' | 'post' | null>('get');
 const loading = ref<boolean>(false);
 const result = ref<string | null>(null);
+const error = ref<string | null>(null);
 
 const onSubmit = async () => {
   if (number.value === null || method.value === null) {
@@ -21,8 +23,16 @@ const onSubmit = async () => {
     if (response.status === 'ok' && response.num_in_english) {
       result.value = response.num_in_english;
     }
-  } catch (error) {
-    console.error(error);
+    if (response.status === 'error' && response.error) {
+      error.value = response.error;
+    }
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      error.value =
+        err.response?.data.error || err.message || 'An error occurred';
+    } else {
+      error.value = 'An error occurred';
+    }
   } finally {
     loading.value = false;
   }
@@ -30,36 +40,59 @@ const onSubmit = async () => {
 </script>
 
 <template>
-  <h1>English numbers</h1>
-  <form @submit.prevent="onSubmit">
-    <label for="number-input">Number:</label>
-    <input
-      id="number-input"
-      v-model="number"
-      type="number"
-      name="number"
-      min="0"
-      max="999999999999999"
-      required
-      placeholder="Enter a number"
-    />
-    <h4>Method:</h4>
-    <span>
-      <input v-model="method" type="radio" id="get" name="method" value="get" />
-      <label for="get">GET</label>
-    </span>
-    <span>
-      <input
-        v-model="method"
-        type="radio"
-        id="post"
-        name="method"
-        value="post"
-      />
-      <label for="post">POST</label>
-    </span>
-    <button type="submit">Submit</button>
-  </form>
-  <p v-if="loading">Loading...</p>
-  <p v-if="!loading && result">{{ result }}</p>
+  <main class="container mx-auto p-4">
+    <h1 class="text-3xl font-bold mb-4">English numbers</h1>
+    <form @submit.prevent="onSubmit" class="space-y-4">
+      <div class="form-control">
+        <label for="number-input" class="label">Number:</label>
+        <input
+          id="number-input"
+          v-model="number"
+          type="number"
+          name="number"
+          min="0"
+          max="999999999999999"
+          required
+          placeholder="Enter a number"
+          class="input input-bordered w-full"
+        />
+      </div>
+      <div class="form-control">
+        <h4 class="label-text">Method:</h4>
+        <label class="label cursor-pointer">
+          <span class="label-text">GET</span>
+          <input
+            v-model="method"
+            type="radio"
+            name="method"
+            value="get"
+            class="radio radio-primary"
+          />
+        </label>
+        <label class="label cursor-pointer">
+          <span class="label-text">POST</span>
+          <input
+            v-model="method"
+            type="radio"
+            name="method"
+            value="post"
+            class="radio radio-primary"
+          />
+        </label>
+      </div>
+      <button class="btn btn-primary w-full" type="submit">Submit</button>
+    </form>
+  </main>
+  <div class="mt-4">
+    <p v-if="!loading && result" class="text-green-500">{{ result }}</p>
+    <p v-if="!loading && error" class="text-red-500">{{ error }}</p>
+  </div>
+  <div
+    v-if="loading"
+    class="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50"
+  >
+    <span class="loading loading-spinner loading-lg text-white"
+      >Loading...</span
+    >
+  </div>
 </template>
